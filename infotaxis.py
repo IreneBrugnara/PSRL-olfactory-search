d27c296f6cfb018a03d2d393f31be48f788b5bff
Irene Brugnara <irene.brugnara@leader.it>
2021-07-19 11:46:52 +0200

    small refactor in infotaxis()

--- infotaxis.py
+++ infotaxis.py
@@ -87,56 +87,50 @@ class Gridworld():
         
         
     def infotaxis(self, t):  # chiamarla policy() oppure advance()
-        # non ho vettorizzato il calcolo perchè ci sono solo 5 elementi da valutare
+        # non ho vettorizzato il calcolo perchè ci sono solo 5 elementi da valutare (only steps of length 1)
         possible_actions = [(0,0), (0,1), (0,-1), (1,0), (-1,0)]  # set of candidate actions (still, right, left, down, up)
         new_states = list(map(self.step, possible_actions))
-        # a dictionary of lists: the keys of the dictionary are the possible new states and the indexes of the lists
-        # are the possible observations
-        new_belief = dict.fromkeys(new_states, [None,None])  # one row for each new_state I could reach and one column for each observation (0 or 1) I could make
-        # new_belief[s][y] is the belief I will have if I go to state s and observe y
+        new_belief = np.empty((5, 2), dtype=object) # one row for each new_state I could reach and one column for each observation (0 or 1) I could make
+        # new_belief[i][y] is the belief I will have if I go to state new_states[i] and observe y
         # oppure potrei fare un dictionary con le keys che sono i new_states
-        new_entropy = dict.fromkeys(new_states, [None,None])
-        delta_S = dict.fromkeys(new_states)
+        new_entropy = np.empty_like(new_belief, dtype=float)
+        delta_S = np.empty(5, dtype=float)
         # predict/anticipate  how the entropy changes in the three possible future scenarios: the source is found
         # in new_state, or source is not found and an observation is made or not
-        for s in new_states:
-            p = self.bernoulli_param_vectorial(s)
+        for i, new_state in enumerate(new_states):
+            p = self.bernoulli_param_vectorial(new_state)
             # p[new_state] = 0 ??????
             mean_p = np.sum(self.belief * p)   # mean bernoulli parameter weighted with the current belief
             for y in [0,1]:  # loop over the possible outcomes
                 lkh = p if y==1 else 1-p   # likelihood
-                lkh[s] = 0
+                lkh[new_state] = 0
                 posterior = self.belief * lkh    # posterior calculation with bayesian update
                 posterior /= np.sum(posterior)   # marginalize
-                
-                new_belief[s][y] = posterior
-                new_entropy[s][y] = - np.sum(posterior[posterior>0] * np.log(posterior[posterior>0]))
+
+                new_belief[i,y] = posterior
+                new_entropy[i,y] = - np.sum(posterior[posterior>0] * np.log(posterior[posterior>0]))
                 # oppure usare scipy.stats.entropy applicato a posterior.flatten()
             # controllare che l'entropia sia sempre positiva!
-            greedy_term = self.belief[s]*(-self.entropy)
-            exploration_term = (1-self.belief[s])*(mean_p*(new_entropy[s][1]-self.entropy) +
-                                                   (1-mean_p)*(new_entropy[s][0]-self.entropy))
-            # non mi piace che [s] viene usato in due modi diversi.... confonde le idee a leggerlo
-            delta_S[s] = greedy_term + exploration_term    # formula (1) del paper
+            greedy_term = self.belief[new_state]*(-self.entropy)
+            exploration_term = (1-self.belief[new_state])*(mean_p*(new_entropy[i,1]-self.entropy) +
+                                                   (1-mean_p)*(new_entropy[i,0]-self.entropy))
+            delta_S[i] = greedy_term + exploration_term    # formula (1) del paper
             # expected entropy variation
 
-        # pick the move that maximizes the expected reduction in entropy
-        #self.state = min(delta_S, key=delta_S.get)
-        best_states = [s for s in new_states if abs(delta_S[s]-min(delta_S.values()))<1e-14]    # delta_S[s]==min(delta_S.values()) is not correct because of numerical floating-point errors
+        # pick the move that maximizes the expected reduction in entropy, randomly breaking ties
+        i = np.random.choice(np.flatnonzero(np.isclose(delta_S,delta_S.min(),atol=1e-13)))   # delta_S==min(delta_S) is not correct because of numerical floating-point errors
+        self.state = new_states[i]
+        #best_states = [s for s in new_states if abs(delta_S[s]-min(delta_S.values()))<1e-14]    
         
-        self.state = best_states[np.random.choice(len(best_states))]   # randomize in case of ties
         self.done = (self.state==self.source)
         actual_obs=0   # solo per il plot
         if not self.done:
             actual_obs = self.observe()
-            self.belief = new_belief[self.state][actual_obs]
-            self.entropy = new_entropy[self.state][actual_obs]
+            self.belief = new_belief[i][actual_obs]
+            self.entropy = new_entropy[i][actual_obs]
             
         if self.render:
             grid.show(t, actual_obs)
-        # ritorno una delle possible_actions
-        
-        # what to do in case of ties? randomizzo anche qua?
         
         
         # non serve che faccio l'update() perchè basta riciclare il calcolo già fatto sui 5
@@ -198,9 +192,9 @@ nrows=201
 ncols=201
 myseed=2
 np.random.seed(myseed) 
-#np.random.seed() 
+np.random.seed() 
 init_state = 140,100
 source = 20,60
-grid=Gridworld(nrows, ncols, source, init_state, render=False, pause=0)
+grid=Gridworld(nrows, ncols, source, init_state, render=True, pause=0)
 print(gridworld_search(grid, wait_first_obs=True))
 
